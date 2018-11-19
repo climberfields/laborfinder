eeaf52fd7741f9076678b96b09165552116b9876
Alex <climberfields@gmail.com>
2018-11-19 14:01:08 -0600

    job start time updated, django admin created, now working on setting up the views and database in the settings

--- laborfinder/labor/models.py
+++ laborfinder/labor/models.py
@@ -6,7 +6,7 @@ class Contractor(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     budget = models.IntegerField(default=100)
-    job_start_time = models.IntegerField(max_length=100)
+    job_start_time = models.IntegerField(default=100)
     pickup_available = models.BooleanField()
     contact_info = models.CharField(max_length=100)
     
