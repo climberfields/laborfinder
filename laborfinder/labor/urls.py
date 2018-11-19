c4c435e23645f3eba272e75ac360838d81afa240
Alex <climberfields@gmail.com>
2018-11-19 17:00:36 -0600

    fixed views. connects to server but it shows a 404 message unable to find the paths. Troubleshooting now

--- laborfinder/labor/urls.py
+++ laborfinder/labor/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Contractor, Contractor_detail
 
 urlpatterns = [
-    path('', Contractor.as_view())
-    path('<int:pk>/', Contractor_detail.as.view())
+    path('', Contractor.as_view()),
+    path('<int:pk>/', Contractor_detail.as_view())
 ]
--- laborfinder/labor/views.py
+++ laborfinder/labor/views.py
@@ -1,51 +1,55 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
-from .modles import Book
+from .models import Contractor
 import json
 
-class Contractor(View)
+class Contractor(View):
 # Create your views here.
 
-def dispatch(self, request, *args, **kwargs):
-    return super(Contractor, self).dispatch(request, *args, **kwargs)
+    def dispatch(self, request, *args, **kwargs):
+        return super(Contractor, self).dispatch(request, *args, **kwargs)
 
-def get(self, request)
+    def get(self, request):
 
-contractor_list = list(Contractor.objects.value())
-    return JsonResponse({
+        contractor_list = list(Contractor.objects.value())
+        return JsonResponse({
         'Content-Type': 'application/json',
         'status': 200,
         'data': contractor_list
-    }, safe=False)
+        }, safe=False)
+    
+class Contractor_detail(View):
+    def dispatch(self, request, *args, **kwargs):
+        return super(Contractor, self).dispatch(request, *args, **kwargs)
 
-def post(self, request):
-    data: request.decode.body('utf-8')
-    print(data)
-    data=json.loads(data)
+    def post(self, request):
+        data: request.decode.body('utf-8')
+        print(data)
+        data=json.loads(data)
     
-    try: 
-        edit_contractor = Contractor.objects.get(pk=pk)
-        data_key = list(data.keys())
-        for key in data_key:
-            if key == "name"
-            edit_contractor.name = data[key]
-            if key == "description"
-            edit_contractor.description = data[key]
-            if key == budget "budget"
-            edit_contractor.budget = data[key]
-            if key == 'job_start_time'
-            edit_contractor.job_start_time = data[key]
-            if key == 'pickup_available'
-            edit_contractor.pickup_available =data[key]
-            if key == 'contact_info'
-            edit_contractor.contact_info = data[key]
-            
-        edit_contractor.save()
-        return JsonResponse({"updated":data},  safe=False)
-    except Contractor.DoesNotExist:
-        return JsonResponse({"Contractor does not exist"})
+        try: 
+            edit_contractor = Contractor.objects.get(pk=pk)
+            data_key = list(data.keys())
+            for key in data_key:
+                if key == "name":
+                    edit_contractor.name = data[key]
+                if key == "description":
+                    edit_contractor.description = data[key]
+                if key == "budget":
+                    edit_contractor.budget = data[key]
+                if key == 'job_start_time':
+                    edit_contractor.job_start_time = data[key]
+                if key == 'pickup_available':
+                    edit_contractor.pickup_available =data[key]
+                if key == 'contact_info':
+                    edit_contractor.contact_info = data[key]
+
+            edit_contractor.save()
+            return JsonResponse({"updated":data},  safe=False)
+        except Contractor.DoesNotExist:
+            return JsonResponse({"Contractor does not exist"})
     
-    def delete(self, requests, pk)
+    def delete(self, requests, pk):
         try: 
             contractor_delete = Contractor.objects.get(pk=pk)
             contractor_delete.delete()
