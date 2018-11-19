668a60a67ec943f73fe59e8913ef18ee3c0100db
Alex <climberfields@gmail.com>
2018-11-19 13:50:34 -0600

    Contractor Schema set up, django DB set up

--- laborfinder/labor/models.py
+++ laborfinder/labor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Contractor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    budget = models.IntegerField(max_length=100)
+    job_start_time = models.IntegerField(max_length=100)
+    pickup_available = models.BooleanField()
+    contact_info = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+        
